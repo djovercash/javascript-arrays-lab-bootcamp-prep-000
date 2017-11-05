858ee33604e0665f79fe66dc7fc9720f154172dd
Drew Overcash <drewovercash@gmail.com>
2017-11-05 14:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function destructivelyAppendKitten (array, element) {
   window.kittens.push('Ralph');
   return window.kittens;
 };
+
+function destructivelyPrependKitten (array, element) {
+  window.kittens.unshift('Bob');
+  return window.kittens;
+};
